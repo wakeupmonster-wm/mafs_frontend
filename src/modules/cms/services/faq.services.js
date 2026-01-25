6f971a5773088e19c7b1e8b637da7d4409833745
Raj Yadav <raj.yadav29.wm@gmail.com>
2026-01-26 00:37:58 +0530

    UPDATE CMS API Endpoints

--- src/modules/cms/services/faq.services.js
+++ src/modules/cms/services/faq.services.js
@@ -1,17 +1,18 @@
+import { CMS_ENDPOINTS } from "@/services/api-enpoints/cms.endpoints";
 import { apiConnector } from "@/services/axios/axios.connector";
 
 export const getAllFAQsAPI = async () => {
-  return apiConnector("GET", "/api/v1/content/faq");
+  return apiConnector("GET", CMS_ENDPOINTS.FAQ_ENDPOINTS.GET_ALL);
 };
 
 export const createFAQAPI = async (payload) => {
-  return apiConnector("POST", "/api/v1/admin/cms/add-faq", payload);
+  return apiConnector("POST", CMS_ENDPOINTS.FAQ_ENDPOINTS.CREATE, payload);
 };
 
 export const updateFAQAPI = async ({ id, payload }) => {
-  return apiConnector("PATCH", `/api/v1/admin/cms/update-faq/${id}`, payload);
+  return apiConnector("PATCH", CMS_ENDPOINTS.FAQ_ENDPOINTS.UPDATE(id), payload);
 };
 
 export const deleteFAQAPI = async (id) => {
-  return apiConnector("DELETE", `/api/v1/admin/cms/delete-faq/${id}`, {});
+  return apiConnector("DELETE", CMS_ENDPOINTS.FAQ_ENDPOINTS.DELETE(id));
 };
--- src/modules/cms/services/privacy.policy.services.js
+++ src/modules/cms/services/privacy.policy.services.js
@@ -1,11 +1,16 @@
+import { CMS_ENDPOINTS } from "@/services/api-enpoints/cms.endpoints";
 import { apiConnector } from "@/services/axios/axios.connector";
 
 // GET the single Privacy Policy document
 export const getPrivacyPolicyAPI = async () => {
-  return apiConnector("GET", "/api/v1/content/privacy-policy");
+  return apiConnector("GET", CMS_ENDPOINTS.PRIVACY_ENDPOINTS.GET_PRIVACYPOLICY);
 };
 
 // UPDATE the Privacy Policy
 export const updatePrivacyPolicyAPI = async (payload) => {
-  return apiConnector("POST", "/api/v1/admin/cms/privacy-policy", payload);
+  return apiConnector(
+    "POST",
+    CMS_ENDPOINTS.PRIVACY_ENDPOINTS.UPDATE_PRIVACYPOLICY,
+    payload
+  );
 };
