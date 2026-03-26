a8569c2633ec9d57c466dfe8c48978f50234e2cb
Raj Yadav <raj.yadav29.wm@gmail.com>
2026-03-26 16:13:14 +0530

    Update base.url.js

--- src/services/api-enpoints/base.url.js
+++ src/services/api-enpoints/base.url.js
@@ -1,8 +1,7 @@
 /*===================== NOTE: MAKE SURE TO ASSIGN BASE_URL IN .ENV FILE =====================*/
+// export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "https://api.matchatfirstswipe.com.au";
-
-// export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // export const BASE_URL = "http://localhost:3001";
 // export const BASE_URL = "https://api.matchatfirstswipe.com.au";
